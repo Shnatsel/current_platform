7a5b1857d4375c268a0bb08c771112563ac7bfc9
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-03-29 21:09:46 +0200

    More concise code, more comments

--- build.rs
+++ build.rs
@@ -3,18 +3,11 @@ use std::env;
 fn main() {
     // Cargo sets the host and target env vars for build scripts, but not crates:
     // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts
-    export_var("HOST_PLATFORM", &query_host_triple());
-    export_var("TARGET_PLATFORM", &query_target_triple());
+    // So we just re-export them to the crate code under a non-clashing name.
+    export_var("HOST_PLATFORM", &env::var("HOST").unwrap());
+    export_var("TARGET_PLATFORM", &env::var("TARGET").unwrap());
 }
 
 fn export_var(name: &str, value: &str) {
     println!("cargo:rustc-env={}={}", name, value);
-}
-
-fn query_target_triple() -> String {
-    env::var("TARGET").unwrap()
-}
-
-fn query_host_triple() -> String {
-    env::var("HOST").unwrap()
 }
