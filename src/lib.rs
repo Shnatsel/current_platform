d49418c8ed79cbe7c419423a14ad26f799ac82e2
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-03-29 21:20:00 +0200

    More detailed documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
-pub const HOST_PLATFORM: &str = env!("HOST_PLATFORM");
+/// The platform on which your code is running.
+/// 
+/// Also known as "target platform".
+pub const CURRENT_PLATFORM: &str = env!("TARGET_PLATFORM");
 
-pub const TARGET_PLATFORM: &str = env!("TARGET_PLATFORM");
+/// The platform on which your code was compiled.
+/// 
+/// Also known as "host platform"
+/// It will only be different from `CURRENT_PLATFORM` if the code was cross-compiled.
+/// This is rarely needed; if in doubt, use `CURRENT_PLATFORM` instead.
+pub const COMPILED_ON: &str = env!("HOST_PLATFORM");
