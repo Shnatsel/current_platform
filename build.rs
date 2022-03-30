27e11c6396e94bfaaf0e82f82036f8df965e7d8a
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-03-30 17:27:47 +0200

    Rerun the build script on TARGET env var change

--- build.rs
+++ build.rs
@@ -6,6 +6,9 @@ fn main() {
     // So we just re-export them to the crate code under a non-clashing name.
     export_var("HOST_PLATFORM", &env::var("HOST").unwrap());
     export_var("TARGET_PLATFORM", &env::var("TARGET").unwrap());
+    // By default Cargo only runs the build script when a file changes.
+    // This should make it re-run on target change (hopefully!)
+     println!("cargo:rerun-if-changed-env=TARGET")
 }
 
 fn export_var(name: &str, value: &str) {
