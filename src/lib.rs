b3d16042b8aee9b0faf7faa50293f5acb67a3b35
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-03-30 18:27:18 +0200

    Fill in description and docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+//! **Find out what platform your code is running on:**
+//!
+//! ```no_run
+//! use current_platform::CURRENT_PLATFORM;
+//! // docs.rs runs on x86 Linux
+//! assert_eq!(CURRENT_PLATFORM, "x86_64-unknown-linux-gnu");
+//! ```
+//!
+//! This information is resolved **at compile time,**
+//! based on the platform for which your code is compiled.
+//! So this crate incurs zero runtime cost.
+//!
+//! This crate is intentionally minimal and only provides the target triple.
+//! You can find out other properties of the platform using crates such as
+//! [`platforms`](https://docs.rs/platforms/latest/platforms/)
+//!  (auto-generated, always up to date) or
+//! [`target-lexicon`](https://docs.rs/target-lexicon/latest/target_lexicon/)
+//! (more detailed but may be missing newly added or obscure platforms).
+
 /// The platform on which your code is running.
 ///
 /// Also known as "target platform".
@@ -6,7 +25,8 @@ pub const CURRENT_PLATFORM: &str = env!("TARGET_PLATFORM");
 /// The platform on which your code was compiled.
 ///
 /// Also known as "host platform".
-/// It will only be different from [`CURRENT_PLATFORM`](CURRENT_PLATFORM) if the code was cross-compiled.
+/// It will only be different from [`CURRENT_PLATFORM`](CURRENT_PLATFORM)
+/// if the code was [cross-compiled.](https://en.wikipedia.org/wiki/Cross_compiler)
 ///
 /// This is rarely needed; if in doubt, use [`CURRENT_PLATFORM`](CURRENT_PLATFORM).
 pub const COMPILED_ON: &str = env!("HOST_PLATFORM");
