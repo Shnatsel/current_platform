4800248dafff6d3d0dcb7591f3dc09b2a3e8e430
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2022-04-14 01:33:15 +0200

    Move build.rs to src/ to reduce the confusion further

--- src/lib.rs
+++ src/lib.rs
@@ -27,12 +27,18 @@
 ///
 /// Also known as "target platform".
 
-// I know what you're thinking:
+// Now you're probably thinking:
 // "I should have simply put `env!` in my binary instead of using this crate!"
-// Not so fast: this variable is only available here because we exported it
-// from build.rs, and trust me, if you have build.rs already used for something,
-// figuring out the re-run logic for your build.rs is gonna be a pain.
-// Hence this crate to provide a separate build.rs with its own self-contained `rerun-if`.
+// But this variable is only available here because we exported it from build.rs,
+// and if you have build.rs already used for something,
+// figuring out the re-run logic for your build.rs is gonna be a massive pain!
+// As soon as you add a `rerun-if` for this enviroment variable,
+// all the default heuristics are disabled and you'll have to recreate them manually:
+// https://github.com/rust-lang/cargo/issues/4587
+//
+// This crate to provide a separate build.rs with its own self-contained `rerun-if`
+// that we've already figured out and tested, and will not trample all over
+// the change detection your own build script relies on.
 pub const CURRENT_PLATFORM: &str = env!("TARGET_PLATFORM");
 
 /// The platform on which your code was compiled.
